8998f0281be77e260858045461cbaa884f712c39
look-at-the-sky <47905050+look-at-the-sky@users.noreply.github.com>
2022-02-10 20:05:30 +0100

    Add files via upload

---
+++ debloat.cmd
@@ -0,0 +1,74 @@
+del /F /Q %WINDIR%\System32\wlidsvc.dll &:: Microsoft Account
+del /F /Q %WINDIR%\System32\AxInstSv.dll &:: ActiveX
+del /F /Q %WINDIR%\System32\AxInstUI.exe &:: ActiveX
+del /F /Q %WINDIR%\System32\BthHFSrv.dll &:: Bluetooth
+del /F /Q %WINDIR%\System32\bthserv.dll &:: Bluetooth
+del /F /Q %WINDIR%\System32\SCPolicySvc.dll &:: SmartCard
+del /F /Q %WINDIR%\System32\WbioSrvc.dll &:: Windows Biometric Service
+del /F /Q %WINDIR%\System32\ScDeviceEnum.dll &:: SmartCard
+del /F /Q %WINDIR%\System32\SCardSvr.dll &:: SmartCard
+del /F /Q %WINDIR%\System32\TabletInputService.dll &:: TabletPC
+del /F /Q %WINDIR%\System32\wcncsvc.dll &:: Windows Connect Now
+del /F /Q %WINDIR%\System32\WSService.dll &:: Windows Store
+del /F /Q %WINDIR%\System32\TermService.dll &:: Remote Desktop
+del /F /Q %WINDIR%\System32\IEEtwCollectorService.dll &:: Internet Explorer
+del /F /Q %WINDIR%\System32\AppXSvc.dll &:: AppX Windows Store
+del /F /Q %WINDIR%\System32\lfsvc.dll &:: Geolocation
+del /F /Q %WINDIR%\System32\TapiSrv.dll &:: Telephony
+del /F /Q %WINDIR%\System32\SnippingTool.exe &:: Useless
+del /F /Q %WINDIR%\System32\SoundRecorder.exe &:: Useless
+del /F /Q %WINDIR%\System32\WSCollect.exe &:: Windows Store logs
+del /F /Q %WINDIR%\System32\WSClient.dll &:: Windows Store Licence
+del /F /Q %WINDIR%\System32\Windows.Devices.SmartCards.dll
+del /F /Q %WINDIR%\System32\Windows.Devices.Geolocation.dll
+del /F /Q %WINDIR%\System32\Windows.Devices.Bluetooth.dll
+del /F /Q %WINDIR%\System32\PurchaseWindowsLicense.dll
+del /F /Q %WINDIR%\System32\PurchaseWindowsLicense.exe
+del /F /Q %WINDIR%\System32\BluetoothApis.dll
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+del /F /Q %WINDIR%\System32\
+
+rmdir /s /q %WINDIR%\System32\ar-SA
+rmdir /s /q %WINDIR%\System32\bg-BG
+rmdir /s /q %WINDIR%\System32\cs-CZ
+rmdir /s /q %WINDIR%\System32\da-DK
+rmdir /s /q %WINDIR%\System32\de-DE
+rmdir /s /q %WINDIR%\System32\el-GR
+rmdir /s /q %WINDIR%\System32\es-ES
+rmdir /s /q %WINDIR%\System32\et-EE
+rmdir /s /q %WINDIR%\System32\fi-FI
+rmdir /s /q %WINDIR%\System32\he-IL
+rmdir /s /q %WINDIR%\System32\hr-HR
+rmdir /s /q %WINDIR%\System32\hu-HU
+rmdir /s /q %WINDIR%\System32\ja-JP
+
+rmdir /s /q %WINDIR%\System32\migwiz &:: Useless
+rmdir /s /q %WINDIR%\System32\Macromed &:: Macromedia Flash
+rmdir /s /q %WINDIR%\Speech &:: Speech Synthesis
+rmdir /s /q %WINDIR%\WinStore &:: Windows Store
+rmdir /s /q %WINDIR%\Camera &:: AppX Camera
+rmdir /s /q %WINDIR%\FileManager &:: SkyDrive aka OneDrive
+rmdir /s /q %WINDIR%\ADFS &:: Server Feature
+rmdir /s /q %WINDIR%\TAPI &:: Telephony API
+
+pause
